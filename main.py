811b6d6947ab0c6dfccee595fd5b477ac0c03765
Anikin Evgeniy <ani.evgeniy@yandex.ru>
2025-11-13 13:12:10 +0300

    stage 2

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import re
 import gzip
 from tempfile import gettempdir
 from urllib import request
+import yedextended as yed
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def error(msg): # Вывод ошибки
 	print(f"Ошибка: {msg}", file=sys.stderr)
@@ -112,15 +115,12 @@ def print_config(args): # Вывод в формате ключ=значение
 			print(f"{k}={v!s}")
 
 def parse_repo_url(url, package):
-	url += "/dists/" + package + "/main/binary-amd64/Packages.gz"
 	try:
-		#prs = request.urlopen(url).read().decode("utf-8")
-		#print(prs)
-		request.urlretrieve(url, gettempdir() + "\\Packages.gz")
+		request.urlretrieve(url + "/Packages.gz", gettempdir() + "\\Packages.gz")
 		with gzip.open(gettempdir() + "\\Packages.gz", "rt", encoding="utf-8") as f:
 			return f.read()
 	except Exception as e:
-		return ""
+		raise ValueError("Invalid repo")
 
 def load_packages(text):
 	packages = {}
@@ -133,15 +133,55 @@ def load_packages(text):
 			packages[name] |= set(deps.split())
 	return packages
 
+def make_graph(root, packages):
+	def dfs(name):
+		graph[name] = set()
+		for dep in packages.get(name, set()):
+			if dep not in graph:
+				dfs(dep)
+			graph[name].add(dep)
+	graph = {}
+	dfs(root)
+	return graph
+
+def viz(graph, main):
+	y = yed.Graph()
+
+	nodes = {main: y.add_node(main)}
+	for name, deb in graph.items():
+		if name == main:
+			continue
+		nodes[name] = y.add_node(name)
+		y.add_edge(nodes[main], nodes[name])
+		for n in deb:
+			nodes[n] = y.add_node(n)
+			y.add_edge(nodes[name], nodes[n])
+	G = nx.DiGraph()
+	for src, deps in graph.items():
+		for dst in deps:
+			G.add_edge(src, dst)
+
+	pos = nx.spring_layout(G, k=0.8, iterations=50)
+	plt.figure(figsize=(8, 6))
+	nx.draw(G, pos, with_labels=True, node_color="lightblue", node_size=1000,
+			font_size=10, arrowsize=15)
+	plt.title("Граф зависимостей пакета", fontsize=14)
+	plt.axis("off")
+	plt.show()
+	#y.persist_graph(f"{main}.graphml")
+
 def main(argv=None):
 	try:
 		args = parse_args(argv)
 		validate_args(args)
 		pars = parse_repo_url(args.repo_url, args.package)
-		if pars:
-			print(load_packages(pars))
+		packs = load_packages(pars)
+		if args.package in packs:
+			graph = make_graph(args.package, packs)
+			print(graph[args.package])
+			#viz(graph, args.package)
 		else:
-			print("Не удалось найти пакет")
+			print("Пакет отсутствует")
 	except SystemExit as e:
 		# argparse использует SystemExit если код != 0 — считаем это ошибкой парсинга
 		if e.code != 0:
@@ -149,6 +189,6 @@ def main(argv=None):
 		else:
 			return
 	except Exception as exc:
-		error(f"Необработанная ошибка: {exc}")
+		error(exc)
 
 main()
